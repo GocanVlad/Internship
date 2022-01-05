55af6533faaea158173384c8eeb5c784fdc2f80f
GocanVlad <vlad.gocan.ext@siemens.com>
2022-01-05 11:39:13 +0200

    api

---
+++ MovieStore/MovieStore/Entities/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieStore.Entities
+{
+    public class Movie 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } 
+        
+        [MaxLength(200)]
+        public string Genre { get; set; }
+        public ICollection<Rating> Ratings { get; set; }
+            = new List<Rating>();
+    }
+}
---
+++ MovieStore/MovieStore/Repository/IMovieRepository.cs
@@ -0,0 +1,23 @@
+﻿using MovieStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieStore.Services
+{
+    public interface IMovieRepository
+    {
+        IEnumerable<Movie> GetMovies();
+        Movie GetMovie(int movieId, bool includeRatings);
+        IEnumerable<Rating> GetRatingForMovie(int movieId);
+        Rating GetRatingForMovie(int movieId, int ratingId);
+        bool MovieExists(int movieId);
+        void AddRatingForMovie(int movieId, Rating rating);
+        bool Save();
+        void DeleteRating(Rating rating);
+        void DeleteMovie(Movie movie);
+        void AddMovie(Movie movie);
+        Movie GetMovie(int movieId);
+    }
+}
